0e4f26b72ad06fb08b8b41456d9402f1594bc561
Travis Fields <tfields@commercehub.com>
2014-01-07 15:28:46 -0500

    Updated spec tests to look for noop scenario

--- spec/classes/windows_puppet_spec.rb
+++ spec/classes/windows_puppet_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe 'windows_puppet' do
-
   shared_context 'puppet' do |version|
     let(:facts){{:puppetversion => version,:operatingsystem => 'windows'}}
   end
@@ -21,22 +20,12 @@ describe 'windows_puppet' do
                                                    })
     }
   end
-
   describe 'ensure no opp for same version' do
-    include_context 'puppet','3.3.1'
+    include_context 'puppet','3.4.1'
     let(:params){{:version => '3.4.1'}}
     it {
-      should contain_pget('DownloadPuppet').with({
-                                                     'source' => 'http://downloads.puppetlabs.com/windows/puppet-3.4.1.msi',
-                                                     'target' => 'C:/software'
-                                                 })
-      should contain_package('UpgradePuppet').with({
-                                                       'description' => 'Bogus Puppet Name to force installer',
-                                                       'source'      => 'C:/software/puppet-3.4.1.msi',
-                                                       'install_options' => '/qn'
-                                                   })
+      should_not contain_pget('DownloadPuppet')
+      should_not contain_package('UpgradePuppet')
     }
   end
-
-
 end
